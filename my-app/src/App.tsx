27ce0ef39fd2d7e548cb90c785c2d1ec33377067
WinZawOo <winzawoo.dev@gmail.com>
2025-03-08 10:11:32 +0000

    test commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR CICD workshop
+          Edit <code>src/App.tsx</code> and save to test HMR CICD workshop lab 3 test
         </p>
       </div>
       <p className="read-the-docs">
